d53034d829f90c2307e6f93a6034d170f1dcd094
Elizabeth <bagiraliza21@gmail.com>
2021-06-22 19:15:37 +0300

    well done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-import './sass/main.scss';
+import './styles.css';
+import menu from './menu.json';
+import createCard from './templates/card.hbs';
+
+const toggleRef = document.querySelector('#theme-switch-toggle');
+const menuList = document.querySelector('.js-menu');
+const bodyRef = document.querySelector('body');
+
+console.log(createCard(menu));
+
+menuList.insertAdjacentHTML('afterbegin', createCard(menu));
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const { LIGHT, DARK } = Theme;
+
+const theme = localStorage.getItem('theme') || LIGHT;
+
+updatePageTheme(theme);
+
+toggleRef.checked = theme === DARK;
+
+toggleRef.addEventListener(`change`, toggleTheme);
+
+function toggleTheme(event) {
+  let theme = LIGHT;
+  if (event.target.checked) {
+    theme = DARK;
+  }
+  localStorage.setItem('theme', theme);
+  updatePageTheme(theme);
+}
+
+function updatePageTheme(theme) {
+  bodyRef.classList = theme;
+}
